f3b4381660d9d8c6547d4056772712c4016728aa
听日要早起 <xujinhuan_email@163.com>
2022-09-03 20:16:45 +0800

    feat:添加gin的翻译器

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module tools
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/kataras/golog v0.1.7
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
---
+++ translation/main.go
@@ -0,0 +1,42 @@
+package translation
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+)
+
+var trans ut.Translator
+
+func InitTranslation(locale string) (err error) {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		zhT := zh.New() //中文翻译器
+		enT := en.New() //英文翻译器
+		//第一个参数是备用的语言环境，后面的参数是应该支持的语言环境
+		uni := ut.New(enT, zhT, enT)
+		trans, ok = uni.GetTranslator(locale)
+		if !ok {
+			return fmt.Errorf("uni.GetTranslator(%s)", locale)
+		}
+
+		switch locale {
+		case "en":
+			en_translations.RegisterDefaultTranslations(v, trans)
+		case "zh":
+			zh_translations.RegisterDefaultTranslations(v, trans)
+		default:
+			en_translations.RegisterDefaultTranslations(v, trans)
+		}
+		return
+	}
+	return
+}
+
+// 使用
+// errs,_ := err.(validator.ValidationErrors)
+// errs.Translate(trans)
