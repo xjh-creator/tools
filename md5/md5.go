42be0b44822085df33ee5a5a2a31616bd31a19af
听日要早起 <xujinhuan_email@163.com>
2022-09-27 20:24:22 +0800

    feat:add md5

---
+++ md5/md5.go
@@ -0,0 +1,13 @@
+package md5
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+)
+
+func genMd5(code string) string {
+	Md5 := md5.New()
+	_, _ = io.WriteString(Md5, code)
+	return hex.EncodeToString(Md5.Sum(nil))
+}
